f0027cb172943437f072bd9464ba7f0e7a970a7e
BaconRemovalUnit <suizsq@qq.com>
2015-03-17 21:15:55 -0400

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 
 
 public class Main {
-
+	public static void main(String[] args)
+	{
+		int x = 300;
+		int y = 300;
+		int unit = 10;
+		int[][] input = {
+				{1,0,0,1,1},
+				{1,0,0,0,1},
+				{1,1,1,1,1}};
+		
+		Maze maze = new Maze(input, unit);
+		
+		JFrame frame = new JFrame("Maze");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+		frame.getContentPane().add(maze);
+		frame.setBackground(Color.BLACK);
+		frame.setSize(x,y);
+		frame.setVisible(true);
+	}
 }
--- src/Maze.java
+++ src/Maze.java
@@ -1,4 +1,69 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Maze {
+import javax.swing.JPanel;
 
+
+public class Maze extends JPanel{
+	//first[] decides X, second[] decides Y
+	boolean[][] maze;
+	int X;
+	int Y;
+	int u;
+	int Width;
+	int Height;
+	
+	Maze(){	
+	}
+
+	Maze(int[][] grid, int u){
+		this.u = u;
+		this.X = grid.length;
+		this.Y = grid[0].length;
+		this.Width = X*u;
+		this.Height = Y*u;
+		maze = new boolean[grid[0].length][grid.length];
+		for(int i=0; i< grid[0].length; i++){
+			for(int j=0; j<grid.length; j++){
+				if(grid[j][i]==0)
+					maze[i][j] = false;
+				else
+					maze[i][j] = true;
+			}
+		}
+	}
+	
+	Maze(boolean[][] grid, int u){
+		this.u = u;
+		this.X = grid.length;
+		this.Y = grid[0].length;
+		this.Width = X*u;
+		this.Height = Y*u;
+		maze = new boolean[grid.length][grid[0].length];
+		for(int i=0; i< grid.length; i++){
+			for(int j=0; j<grid[0].length; j++){
+					maze[i][j] = grid[i][j];
+			}
+		}
+	}
+	
+	public void paintComponent (Graphics g)
+	{
+		g.setColor(Color.black);
+		g.fillRect(0, 0, X*u, Y*u);
+		for(int i=0; i< maze.length; i++){
+			for(int j=0; j<maze[0].length; j++){
+					if(!maze[i][j]){
+						g.setColor(Color.WHITE);
+						g.fillRect(i*u, j*u, u, u);
+					}
+			}
+		}
+	}
+	
+	private boolean[][] twist(boolean[][] maze){
+		return null;
+	}
+	
+	
 }
